562f7e7bf0186b083504965ea9f001d36b087aa7
juan beresiarte <juanberesiarte@gmail.com>
2024-12-11 12:36:50 -0300

    Use zod in enviroment

--- backend/config/env.ts
+++ backend/config/env.ts
@@ -4,14 +4,17 @@ import { z } from "zod";
 config();
 
 const environment = z.object({
-    PORT: z.string(),
-    IS_DEVELOPMENT: z.string()
+    PORT: z.string().transform(Number),
+    IS_DEVELOPMENT: z.string().transform((val) => val === "true" || val === "1")
 });
 
 export default function env() {
     const safe_env = environment.safeParse(process.env);
 
-    if (!safe_env.success) throw Error("Error en tu entorno(env)");
+    if (!safe_env.success) {
+        console.error("Error en tus variables de entorno:", safe_env.error.format());
+        throw new Error("Error al validar las variables de entorno.");
+    }
 
     return safe_env.data;
 }
