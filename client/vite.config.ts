09bc001d215ad9ca9e69daabec8f3508ae6f6c55
juan beresiarte <juanberesiarte@gmail.com>
2025-01-02 01:06:48 -0300

    Config proxy in development mode

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import env from "../src/config/env";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,5 +8,14 @@ export default defineConfig({
   build: {
     outDir: '../dist/public',
     emptyOutDir: true
+  },
+  server: {
+    proxy: env.MODE === "development" ? {
+      "/api": {
+        target: `htpp://localhost:${env.PORT}`,
+        changeOrigin: true,
+        secure: false
+      }
+    } : {}
   }
 })
